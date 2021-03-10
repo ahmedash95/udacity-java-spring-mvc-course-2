bda4e9b100c34d0758bcb47babdadbc60ae6c567
Ahmed Ashraf <ahmed29329@gmail.com>
2021-03-10 20:57:35 +0100

    base & authentication

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/AuthController.java
@@ -0,0 +1,54 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import com.udacity.jwdnd.course1.cloudstorage.models.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
+    public String loginView() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String registerView() {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupUser(@ModelAttribute User user, Model model) {
+        String signupError = null;
+
+        if (!userService.isUsernameAvailable(user.getUsername())) {
+            signupError = "The username already exists.";
+        }
+
+        if (signupError == null) {
+            int rowsAdded = userService.createUser(user);
+            if (rowsAdded < 0) {
+                signupError = "There was an error signing you up. Please try again.";
+            }
+        }
+
+        if (signupError == null) {
+            model.addAttribute("signupSuccess", true);
+        } else {
+            model.addAttribute("signupError", signupError);
+        }
+
+        return "signup";
+    }
+}
