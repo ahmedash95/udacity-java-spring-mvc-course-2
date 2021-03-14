57840e36e63af68d28fae80c244d95ab1c43bf01
Ahmed Ashraf <ahmed29329@gmail.com>
2021-03-14 12:28:22 +0100

    handle file upload

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/exceptions/ApplicationExceptionHandler.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/exceptions/ApplicationExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.exceptions;
 
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
@@ -11,5 +14,13 @@ public class ApplicationExceptionHandler {
     public String handleApplicationExceptions(ApplicationException e, RedirectAttributes redirectAttributes) {
         redirectAttributes.addAttribute("error", e.getMessage());
         return "redirect:/error";
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleFileUploadExceptions(MaxUploadSizeExceededException e, RedirectAttributes redirectAttributes) {
+        redirectAttributes
+                .addFlashAttribute("message", "File is too big. max size is: 2MB")
+                .addFlashAttribute("message_type", "danger");
+        return "redirect:/home?files";
     }
 }
